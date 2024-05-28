456085770f68c3b5f69fa264cae151af211b234f
ramandeepformay <ramandeepformay@gmail.com>
2024-05-27 23:29:06 -0600

    fixed the blog url issue

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
 // export const BACKEND_URL_USER ="http://localhost:8787/api/v1/user";
 // export const BACKEND_URL_BLOG = "http://localhost:8787/api/v1/blog";
 export const BACKEND_URL_USER = "https://backend.ramandeepformay.workers.dev/api/v1/user";
-export const BACKEND_URL_BLOG = "https://backend.ramandeepformay.workers.dev//api/v1/blog";
+export const BACKEND_URL_BLOG = "https://backend.ramandeepformay.workers.dev/api/v1/blog";
